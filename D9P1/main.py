36579810188ed5f563313108c02e1bbcfcc9a8dd
PriyankaDev077 <ikori41@gmail.com>
2025-01-27 19:44:52 +0530

    D9P3

---
+++ D9P1/main.py
@@ -0,0 +1,16 @@
+#Take input of a word
+string = input("please entre your own word : ")
+#take  input of a character
+char = input("please enter your own character : ")
+i = 0
+count = 0
+#loop will to find the occurence of the character
+while(i < len(string)): #string operation
+
+    if(string[i] == char): #condition1
+        count = count + 1
+        i = i + 1
+
+#display the result
+print("the total number of times" , char, "has occured = " , count)
+
